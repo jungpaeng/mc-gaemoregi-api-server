957a43e1eea3da899d8135a1f4ba0dcb1ee732ac
jungpaeng <meis1541@naver.com>
2019-12-28 01:07:40 +0900

    ssh 인증 기반 화이트리스트 추가/제거 기능 개발

--- src/api/whitelist/index.js
+++ src/api/whitelist/index.js
@@ -1,7 +1,12 @@
 const Router = require('koa-router');
-const whitelist = new Router();
+const koaBody  = require('koa-body');
+
 const whitelistCtrl = require('./whitelist.controller');
 
+const whitelist = new Router();
+
 whitelist.get('/', whitelistCtrl.list);
+whitelist.post('/', koaBody(), whitelistCtrl.create);
+whitelist.del('/:id', whitelistCtrl.delete);
 
 module.exports = whitelist;
--- src/api/whitelist/whitelist.controller.js
+++ src/api/whitelist/whitelist.controller.js
@@ -1,3 +1,67 @@
+const fs = require('fs');
+const nodeSSH = require('node-ssh');
+const ssh = new nodeSSH();
+
+const getJSONFileSystem = (file) => {
+    const jsonFile = fs.readFileSync(file, 'utf8');
+
+    return JSON.parse(jsonFile);
+};
+
+const WHITELIST_FILE = 'C:\\Dev\\MineCraft\\Bukkit\\whitelist.json';
+
 exports.list = (ctx) => {
-    ctx.body = 'listed';
+    try {
+        const jsonFile = getJSONFileSystem(WHITELIST_FILE);
+
+        ctx.status = 200;
+        ctx.body = {
+            data: {
+                results: jsonFile,
+            },
+        };
+    } catch (e) {
+        ctx.status = 500;
+        ctx.body = { message: 'WhiteList File not found' };
+    }
+};
+
+const sshSimpleExec = async (command) => {
+    await ssh.connect({
+        host: '125.141.133.20',
+        username: 'ubuntu',
+        port: '22',
+        password : 'xhdzmsdkdl_meis1541',
+        readyTimeout : 360000
+    });
+
+
+    ssh.exec(`screen -x minecraft -X stuff "${command}"`)
+        .then(res => {
+            ssh.exec('screen -x minecraft -X eval "stuff \\015"');
+        });
+};
+
+exports.delete = async (ctx) => {
+    const {id} = ctx.params;
+
+    try {
+        await sshSimpleExec(`whitelist remove ${id}`);
+        ctx.body = { message: 'Whitelist Deleted!' };
+    } catch (e) {
+        ctx.status = 500;
+        ctx.body = { message: 'Unknown Error in ssh connected' };
+    }
+};
+
+exports.create = async (ctx) => {
+    const {name} = ctx.request.body;
+
+    try {
+        await sshSimpleExec(`whitelist add ${name}`);
+        ctx.body = { message: 'Whitelist Add!' };
+    } catch (e) {
+        ctx.status = 500;
+        ctx.body = { message: 'Unknown Error in ssh connected' };
+    }
 };
