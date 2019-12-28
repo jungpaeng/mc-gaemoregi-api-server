a19b25a9a329b91af74ab140955747dd7117a5dc
jungpaeng <meis1541@naver.com>
2019-12-28 13:35:26 +0900

    cors 패키지 적용

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const Koa = require('koa');
+const cors = require('@koa/cors');
 const Router = require('koa-router');
 
 const api = require('./api');
@@ -8,7 +9,7 @@ const router = new Router();
 
 router.use('/api', api.routes());
 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(cors()).use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, "0.0.0.0", () => {
    console.log('test server is listening to port 4000')
